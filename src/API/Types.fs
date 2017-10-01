10bbc089487ee54d5992b3700a09ef1e5f03c446
Szymon Sasin <szymon@sasin.eu>
2017-10-01 10:19:11 +0100

    Added simple test to detect is object is created properly

---
+++ src/API/Types.fs
@@ -0,0 +1,17 @@
+﻿namespace Automated
+
+module Types =
+    open System
+    
+    type IRequest =
+        abstract member Execute: unit -> string
+
+    type WithQueryString(uri: Uri, method: string, queryString: string) =
+        interface IRequest with 
+            member this.Execute (): string = 
+                 String.Empty
+
+    type WithBody(uri: Uri, method: string, body: string) =
+        interface IRequest with 
+            member this.Execute (): string = 
+                 String.Empty
---
+++ test/API.Test.CSharp/HappyPathUsage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using static Automated.Types;
+
+namespace Automated.API.Test.CSharp
+{
+    public sealed class HappyPathUsage
+    {
+        public HappyPathUsage()
+        {
+
+        }
+
+        [Fact]
+        public void ApiCheckCanCreateRequest()
+        {
+            IRequest request = ApiCheck
+                .WithString("https://testwebhooks.com/c/Automated.ApiCheck")
+                .WithMethod("POST")
+                .WithPayload("{property:'test'}");
+
+            Assert.NotNull(request);
+        }
+    }
+}
