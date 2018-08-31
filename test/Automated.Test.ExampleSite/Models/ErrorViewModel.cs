974079b5ddb222dc6a51893bdccf4c1e061ae37a
Szymon M Sasin <szymon.sasin@vigilantapps.com>
2018-08-31 15:59:05 +0100

    added sample project for testing test project

---
+++ test/Automated.Test.ExampleSite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Automated.Test.ExampleSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
